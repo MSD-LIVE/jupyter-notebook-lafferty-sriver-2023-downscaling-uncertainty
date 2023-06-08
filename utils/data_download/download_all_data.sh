10211bec19231c21610a69892c5fedd55a401454
david0811 <davidcl2@illinois.edu>
2023-06-08 16:56:26 -0500

    data download script

--- utils/data_download/download_all_data.sh
+++ utils/data_download/download_all_data.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
- wget -P ../../data/simplemaps_worldcities_basicv1.76 -i simplemaps.txt
- wget -P ../../data/netcdf -i netcdf_files.txt
+wget -P ../../data/netcdf -i netcdf_files.txt
 
- wget -P ../../data/ https://mpctransfer.blob.core.windows.net/msd-live-lafferty-sriver/data/csv_zipped.zip
- unzip ../../data/csv_zipped.zip -d ../../data/
- rm ../../data/csv_zipped.zip
+wget -P ../../data/ https://mpctransfer.blob.core.windows.net/msd-live-lafferty-sriver/data/csv_zipped.zip
+unzip ../../data/csv_zipped.zip -d ../../data/
+rm ../../data/csv_zipped.zip
  

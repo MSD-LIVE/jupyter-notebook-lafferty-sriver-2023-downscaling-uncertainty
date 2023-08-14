18bd99bab95e72e629c2ecc4f3271bf36c731fd6
David Lafferty <davidcl2@illinois.edu>
2023-08-14 12:06:54 -0500

    Update download_all_data.sh

--- utils/data_download/download_all_data.sh
+++ utils/data_download/download_all_data.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-wget -P ../../data/netcdf -i netcdf_files.txt
+### Download netcdf files
+# Already done!
+# wget -P ../../data/netcdf -i netcdf_files.txt 
 
+### Download csv files for city data
 wget -P ../../data/ https://mpctransfer.blob.core.windows.net/msd-live-lafferty-sriver/data/csv_zipped.zip
 unzip ../../data/csv_zipped.zip -d ../../data/
 rm ../../data/csv_zipped.zip
- 
